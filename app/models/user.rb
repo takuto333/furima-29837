44e54d42720b2c7f61ebb4536ecd5cf5fe19541c
yasukin <70310523+yasukin@users.noreply.github.com>
2020-09-17 19:08:49 +0900

    userモデルのバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :last_name_kanji
+    validates :first_name_kanji
+    validates :last_name_katakana
+    validates :first_name_katakana
+    validates :birthday
+  end       
 end

5f738f02f0b1ddf8b911651d9844a2113669ebcb
yasukin <70310523+yasukin@users.noreply.github.com>
2020-09-17 20:29:03 +0900

    userモデルのバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,10 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :last_name_kanji
-    validates :first_name_kanji
-    validates :last_name_katakana
-    validates :first_name_katakana
+    validates :last_name_kanji, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角を使用してください"}
+    validates :first_name_kanji, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角を使用してください"}
+    validates :last_name_katakana, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カナを使用してください"}
+    validates :first_name_katakana, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カナを使用してください"}
     validates :birthday
   end       
 end

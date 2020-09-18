4585071f277c12e5ddd6b3dc0fc7799048ef7e6b
yasukin <70310523+yasukin@users.noreply.github.com>
2020-09-18 18:31:59 +0900

    スペルミス修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
-  belongs_to_active_hash :shipping_charges
+  belongs_to_active_hash :shipping_charge
   belongs_to_active_hash :shipping_area
   belongs_to_active_hash :days_to_ship
 

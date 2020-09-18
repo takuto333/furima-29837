281785225024a71c6edddc4a5bc9a5da68b83d34
yasukin <70310523+yasukin@users.noreply.github.com>
2020-09-18 14:58:08 +0900

    アソシエーション、バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+has_one_attached :image
+belongs_to :user
+
+with_options presence: true do
+  validates :name
+  validates :category_id
+  validates :condition_id
+  validates :shipping_charges_id
+  validates :shipping_area_id
+  validates :days_to_ship_id
+  validates :description
+  validates :price
+end
 end

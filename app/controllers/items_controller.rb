d0ccaa4372954203a89091087ab0513d8c094ca6
yasukin <70310523+yasukin@users.noreply.github.com>
2020-09-19 08:07:42 +0900

    商品の保存機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,16 @@ class ItemsController < ApplicationController
   def new
     
   end
+
+  def create
+    Item.create(item_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def item_params
+    params.permit(:image, :name, :description, :category_id, :condition_id, :shipping_charges_id, :shipping_area_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
+  end
+
 end

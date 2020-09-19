1d9ccc3a929b24bc936caf23b10672f95c6f605e
yasukin <70310523+yasukin@users.noreply.github.com>
2020-09-19 15:51:41 +0900

    itemモデルの単体テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,6 +10,7 @@ has_one_attached :image
 belongs_to :user
 
   with_options presence: true do
+    validates :image
     validates :name, length: {maximum: 40}
     validates :category_id
     validates :condition_id
@@ -17,7 +18,7 @@ belongs_to :user
     validates :shipping_area_id
     validates :days_to_ship_id
     validates :description, length: {maximum: 1000} 
-    validates :price, :numericality => { :greater_than_or_equal_to => 300 }, :numericality => { :less_than_or_equal_to => 9999999 }
+    validates :price, :numericality => { :greater_than_or_equal_to => 300, :less_than_or_equal_to => 9999999 }
   end
 
   with_options numericality: { other_than: 1 } do
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,78 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品機能' do
+    context '商品出品がうまくいくとき' do
+      it 'image,name,description,category_id,condition_id,shipping_charges_id,shipping_area_id,days_to_ship_id,priceが存在すれば登録できる。' do
+      expect(@item).to be_valid
+      end
+    end
+
+    context '商品出品がうまくいかないとき' do
+      it 'imageがなければ登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'nameがなければ登録できない' do
+        @item.name = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'descriptionがなければ登録できない'do
+        @item.description = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+      it 'category_idが1だと登録できない' do
+        @item.category_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
+      it 'condition_idが1だと登録できない' do
+        @item.condition_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition must be other than 1")
+      end
+      it 'shipping_charges_idが1だと登録できない' do
+        @item.shipping_charges_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping charges must be other than 1")
+      end
+      it 'shipping_area_idが1だと登録できない' do
+        @item.shipping_area_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping area must be other than 1")
+      end
+      it 'days_to_ship_idが1だと登録できない' do
+        @item.days_to_ship_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Days to ship must be other than 1")
+      end
+      it 'priceがなければ登録できない' do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'priceが299円以下だと登録できない'do
+        @item.price = "299"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it 'priceが10000000円以上だと登録できない' do
+        @item.price = "10000000"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+      it 'priceが半角数字以外だと保存されない' do
+        @item.price = "３００"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+    end
+  end
 end

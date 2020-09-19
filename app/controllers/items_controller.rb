ef4a4e13c970128e5a30a49460e5a663ddd66dc9
yasukin <70310523+yasukin@users.noreply.github.com>
2020-09-19 16:56:47 +0900

    rubocopで自動修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,5 +20,4 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_charges_id, :shipping_area_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
   end
-
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -6,26 +6,26 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_area
   belongs_to_active_hash :days_to_ship
 
-has_one_attached :image
-belongs_to :user
+  has_one_attached :image
+  belongs_to :user
 
   with_options presence: true do
     validates :image
-    validates :name, length: {maximum: 40}
+    validates :name, length: { maximum: 40 }
     validates :category_id
     validates :condition_id
     validates :shipping_charges_id
     validates :shipping_area_id
     validates :days_to_ship_id
-    validates :description, length: {maximum: 1000} 
-    validates :price, :numericality => { :greater_than_or_equal_to => 300, :less_than_or_equal_to => 9999999 }
+    validates :description, length: {maximum: 1000}
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 
   with_options numericality: { other_than: 1 } do
-  validates :category_id
-  validates :condition_id
-  validates :shipping_charges_id
-  validates :shipping_area_id
-  validates :days_to_ship_id
+    validates :category_id
+    validates :condition_id
+    validates :shipping_charges_id
+    validates :shipping_area_id
+    validates :days_to_ship_id
   end
 end

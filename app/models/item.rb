b05bfa5d0a86b04fd2161d8617a55591604400ae
yasukin <70310523+yasukin@users.noreply.github.com>
2020-09-24 12:39:26 +0900

    配送先入力欄の設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -22,7 +22,7 @@ class Item < ApplicationRecord
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 
-  with_options numericality: { other_than: 1 } do
+  with_options numericality: { other_than: 0 } do
     validates :category_id
     validates :condition_id
     validates :shipping_charge_id

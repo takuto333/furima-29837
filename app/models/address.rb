208da520bc2c962ed364ed60639dba37ec0d2952
yasukin <70310523+yasukin@users.noreply.github.com>
2020-09-24 12:38:33 +0900

    addressモデルのバリデーションの設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,13 @@
 class Address < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
   belongs_to :order
+
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :prefecture_id, numericality: { other_than: 1 } 
+    validates :municipality
+    validates :address
+    validates :phone_number
+  end
 end

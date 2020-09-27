44dbdacd5ed15f1533161f20532867ad1419de49
yasukin <70310523+yasukin@users.noreply.github.com>
2020-09-27 10:23:48 +0900

    購入機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,34 @@
 class OrdersController < ApplicationController
 
   def index
+    @order = OrderAddress.new
   end
 
   def create
+    # binding.pry
+    @order = OrderAddress.new(order_params)
+    if @order.valid?
+      pay_item
+      @order.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def order_params
+    params.permit(:token, :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id) 
+  end
+
+
+  def pay_item
+    @item = Item.find(params[:item_id])
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency:'jpy'
+    )
   end
 end

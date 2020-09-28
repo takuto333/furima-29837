f3e365e7914d67a05d903450c7626564ed500473
yasukin <70310523+yasukin@users.noreply.github.com>
2020-09-28 16:21:20 +0900

    tokenが空では登録できないように設定

--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -3,6 +3,7 @@ class OrderAddress
   attr_accessor :token, :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :item_id, :user_id
 
   with_options presence: true do
+    validates :token
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
     validates :prefecture_id, numericality: { other_than: 1 }
     validates :municipality

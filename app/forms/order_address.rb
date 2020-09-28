5b03d26eeecab7a0e87ebfe626271141f14ef965
yasukin <70310523+yasukin@users.noreply.github.com>
2020-09-28 16:22:35 +0900

    prefecture_idのバリデーションの修正

--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -5,7 +5,7 @@ class OrderAddress
   with_options presence: true do
     validates :token
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
-    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :prefecture_id, numericality: { other_than: 0 }
     validates :municipality
     validates :address
     validates :phone_number, format: { with: /\A\d{11}\z/ }
